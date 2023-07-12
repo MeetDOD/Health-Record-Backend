ea8b07e9630fce6078817a27d3349c7c3120ac70
Meet Ashok Dodiya <120380073+MeetDOD@users.noreply.github.com>
2023-07-12 18:53:26 +0530

    Update doctorModel.js

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -24,7 +24,7 @@ const doctorModel = mongoose.Schema({
     medRecords: [{
         type:mongoose.Schema.Types.ObjectId,
         required:true,
-        ref:[MedRecord]
+        ref:'MedRecord'
     }]
 })
 
